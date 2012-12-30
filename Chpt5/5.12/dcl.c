34567f5e7eb95d63719784c4e863669ecb12d897
renteng <ren_teng@sina.com>
2012-12-30 16:52:03 +0800

    complete

--- Chpt5/5.12/dcl.c
+++ Chpt5/5.12/dcl.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 #include<string.h>
 #include<ctype.h>
+#include"../../comm/getch.h"
 
 #define MAXTOKEN 100
 
@@ -33,5 +34,94 @@ int main(int argc, char *argv[])
 
 int gettoken()
 {
-	int c, getch
+	int c;
+	char *p = token;
+
+	while((c = getch()) == ' ' || c == '\t');
+		;
+
+	if(c == '(')
+	{
+		if((c = getch()) == ')')
+		{
+			strcpy(token, "()");
+			return tokentype = PARENS;
+		}
+		else
+		{
+			ungetch(c);
+			return tokentype = '(';
+		}
+	}
+	else if(c == '[')
+	{
+		for(*p++ = c;(*p++ = getch()) != ']';)
+			;
+		*p = '\0';
+		return tokentype = BRACKETS;
+	}
+	else if(isalpha(c))
+	{
+		for(*p++ = c;isalnum(c = getch());)
+		{
+			*p++ = c;
+		}
+		*p = '\0';
+		ungetch(c);
+		return tokentype = NAME;
+	}
+	else
+	{
+		return tokentype = c;
+	}
+}
+
+void dcl(void)
+{
+	int ns;
+
+	for(ns = 0;gettoken() == '*';)
+	{
+		ns++;
+	}
+	dirdcl();
+	while(ns--)
+	{
+		strcat(out, " point to");
+	}
+}
+
+void dirdcl(void)
+{
+	int type;
+
+	if(tokentype == '(')
+	{
+		dcl();
+		if(tokentype != ')')
+		{
+			printf("error missing )\n");
+		}
+	}
+	else if(tokentype == NAME)
+	{
+		strcpy(name, token);
+	}
+	else
+	{
+		printf("error:expected name or (dcl)\n");
+	}
+	while((type = gettoken()) == PARENS || type == BRACKETS)
+	{
+		if(type == PARENS)
+		{
+			strcat(out, " function returning");
+		}
+		else
+		{
+			strcat(out, " array");
+			strcat(out, token);
+			strcat(out, " of");
+		}
+	}
 }
